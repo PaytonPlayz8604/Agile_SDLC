9f7eb6af5ee323a9f792a73bfd0a5f7768934daf
Payton <92261180+PaytonPlayz8604@users.noreply.github.com>
2025-01-15 13:33:39 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -3,3 +3,5 @@ fetch('navbar.html')
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
     });
+
+console.log("Welcome, this website provides information about the SDLC model named Agile. Also, hello Mr. French!")
